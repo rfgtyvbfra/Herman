e1bc54fddbf24f93a9e5094562c55f22c50170e5
chelseyfadri <chelseyfadri@gmail.com>
2023-06-04 23:36:57 +0800

    feat: add core feature increment v0

---
+++ internal/execution/evm.go
@@ -0,0 +1,19 @@
+package execution
+
+// EVMEngine v0 provides a mock execution engine.
+type EVMEngineV0 struct{}
+
+func (EVMEngineV0) Init(vm VMType) error {
+    if vm != VM_EVM {
+        return fmt.Errorf("unsupported VM: %s", vm)
+    }
+    return nil
+}
+
+func (EVMEngineV0) Execute(tx []byte) ([]byte, error) {
+    // Mock: echo back with prefix
+    out := append([]byte("evm0-"), tx...)
+    return out, nil
+}
+
+import "fmt"
