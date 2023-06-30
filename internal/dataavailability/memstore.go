0bcd92bbc9e51cf16433b705da2113e5116d807c
rfgtyvbfra <rfgtyvbfras@gmail.com>
2023-06-30 23:25:36 +0800

    feat: add core feature increment v7

--- internal/dataavailability/memstore.go
+++ internal/dataavailability/memstore.go
@@ -24,3 +24,27 @@ func (m *MemStoreV3) Sample(cid string, n int) ([][]byte, error) {
 }
 
 import "fmt"
+package dataavailability
+
+// MemStoreV7 stores chunks in-memory for tests.
+type MemStoreV7 struct{
+    data map[string][]byte
+}
+
+func (m *MemStoreV7) Store(chunk []byte) (string, error) {
+    if m.data == nil { m.data = make(map[string][]byte) }
+    key := fmt.Sprintf("cid-%d", len(m.data)+7)
+    m.data[key] = chunk
+    return key, nil
+}
+
+func (m *MemStoreV7) Sample(cid string, n int) ([][]byte, error) {
+    if n <= 0 {
+        return nil, fmt.Errorf("n must be > 0")
+    }
+    out := make([][]byte, n)
+    for i := 0; i < n; i++ {
+        out[i] = []byte(cid)
+    }
+    return out, nil
+}
