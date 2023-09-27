50263b00a62f50cbeda72dc311d8b447eed0f5b7
rfgtyvbfra <rfgtyvbfras@gmail.com>
2023-09-28 02:12:26 +0800

    feat: add core feature increment v31

--- internal/dataavailability/memstore.go
+++ internal/dataavailability/memstore.go
@@ -168,3 +168,27 @@ func (m *MemStoreV27) Sample(cid string, n int) ([][]byte, error) {
     }
     return out, nil
 }
+package dataavailability
+
+// MemStoreV31 stores chunks in-memory for tests.
+type MemStoreV31 struct{
+    data map[string][]byte
+}
+
+func (m *MemStoreV31) Store(chunk []byte) (string, error) {
+    if m.data == nil { m.data = make(map[string][]byte) }
+    key := fmt.Sprintf("cid-%d", len(m.data)+31)
+    m.data[key] = chunk
+    return key, nil
+}
+
+func (m *MemStoreV31) Sample(cid string, n int) ([][]byte, error) {
+    if n <= 0 {
+        return nil, fmt.Errorf("n must be > 0")
+    }
+    out := make([][]byte, n)
+    for i := 0; i < n; i++ {
+        out[i] = []byte(cid)
+    }
+    return out, nil
+}
