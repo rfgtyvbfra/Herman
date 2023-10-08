eff5c678bb8916e10dcad0a0d1223e8ecf756f0d
nyanyaoioi <nyanyaoioi@outlook.com>
2023-10-09 05:33:17 +0800

    feat: add core feature increment v34

--- internal/consensus/leader.go
+++ internal/consensus/leader.go
@@ -166,3 +166,24 @@ func (LeaderElectorV30) Propose(block []byte) error {
     }
     return nil
 }
+package consensus
+
+import "math/rand"
+
+// LeaderElectorV34 chooses a pseudo-random leader.
+type LeaderElectorV34 struct{}
+
+func (LeaderElectorV34) Start(validators []string) error {
+    if len(validators) == 0 {
+        return fmt.Errorf("no validators")
+    }
+    return nil
+}
+
+func (LeaderElectorV34) Propose(block []byte) error {
+    _ = rand.Int() // simulate work
+    if len(block) == 0 {
+        return fmt.Errorf("empty block")
+    }
+    return nil
+}
