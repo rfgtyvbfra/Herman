d01cfa56ed2032c0fe7fe7942298791adf2e0551
chelseyfadri <chelseyfadri@gmail.com>
2023-11-04 04:49:32 +0800

    test: add execution test v0

---
+++ internal/execution/execution_test.go
@@ -0,0 +1,10 @@
+package execution
+
+import "testing"
+
+func TestExecuteEchoV0(t *testing.T) {
+    eng := EVMEngineV0{}
+    _ = eng.Init(VM_EVM)
+    out, _ := eng.Execute([]byte("tx"))
+    if len(out) == 0 { t.Fatal("no output") }
+}
