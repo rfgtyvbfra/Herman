8ceefcdfe9870c1448e17eab40ad0ff2c867d58e
rfgtyvbfra <rfgtyvbfras@gmail.com>
2023-11-15 06:33:10 +0800

    fix: minor optimization and helper v3

--- internal/execution/helpers.go
+++ internal/execution/helpers.go
@@ -5,3 +5,10 @@ func checksumV1(b []byte) int {
     for _, v := range b { s += int(v) }
     return s
 }
+package execution
+
+func checksumV3(b []byte) int {
+    s := 0
+    for _, v := range b { s += int(v) }
+    return s
+}
