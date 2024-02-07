1266358233118ba2cb3bb062a3bf78ceeb04a57c
rfgtyvbfra <rfgtyvbfras@gmail.com>
2024-02-08 03:47:43 +0800

    test: add execution test v26

--- internal/execution/execution_test.go
+++ internal/execution/execution_test.go
@@ -2,8 +2,8 @@ package execution
 
 import "testing"
 
-func TestExecuteEchoV24(t *testing.T) {
-    eng := EVMEngineV24{}
+func TestExecuteEchoV26(t *testing.T) {
+    eng := EVMEngineV26{}
     _ = eng.Init(VM_EVM)
     out, _ := eng.Execute([]byte("tx"))
     if len(out) == 0 { t.Fatal("no output") }
